db5663637f5232a7951af6bbe1b12bddd8fd7563
Ashwani Dubey <ashwani786dubey@gmail.com>
2021-06-20 16:11:43 +0530

    done

--- src/Showsingle.js
+++ src/Showsingle.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Showsingle({state,confirmed,active,recovered,deaths}) {
     return (
         <div id="card">
-            <h1 id="heading">{state} </h1>
-            <h2> confirmed:{confirmed}</h2>
-            <h2> active:{active}</h2>
-            <h2> recovered:{recovered}</h2>
-            <h2> deaths:{deaths}</h2>
+            <h2 id="heading">{state} </h2>
+            <h3> confirmed : {confirmed}</h3>
+            <h3> active : {active}</h3>
+            <h3> recovered : {recovered}</h3>
+            <h3> deaths : {deaths}</h3>
         </div>
     )
 }
